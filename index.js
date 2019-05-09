cf4ee139cc8e2b8c33cd785a36c5385b3c0118b5
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-09 15:52:45 +0800

    back to 8081

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json('application/json'));
 app.use(cors({credentials: true, origin: true}));
 
 // starting server
-const server = app.listen(80, function () {
+const server = app.listen(8081, function () {
   let host = server.address().address;
   let port = server.address().port;
   console.log("Server listening at http://%s:%s", host, port);
-}); 
+});
 
 // recall set of app router
 require('./views/index')(app);
