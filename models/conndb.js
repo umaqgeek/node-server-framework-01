c8ccbec31b7018866535e3c53281bab826356230
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-09 16:06:11 +0800

    increase timeout

--- models/conndb.js
+++ models/conndb.js
@@ -7,6 +7,7 @@ const con = mysql.createConnection({
   user: process.env.USERNAME, // "root", // your db username
   password: process.env.PASSWORD, // "", // your db password
   database: process.env.DATABASE, // "project_node_db" // your db name
+  acquireTimeout: 1000000,
 });
 
 // establish connection database
