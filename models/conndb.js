a6225d1a9ed53bcc516d21ffa1fe224752dc77c3
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-09 16:59:37 +0800

    fix

--- models/conndb.js
+++ models/conndb.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 // create connection config
 const con = mysql.createConnection({
   host: process.env.HOSTNAME || "localhost", // your db host (type full url if from other server)
-  port: process.env.PORT || 3306, // your db port number (default is 3306)
+  port: 3306, // your db port number (default is 3306)
   user: process.env.USERNAME || "root", // your db username
   password: process.env.PASSWORD || "", // your db password
   database: process.env.DATABASE || "project_node_db", // your db name
