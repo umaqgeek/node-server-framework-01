c3d0cf345d027753f129f77076277a787e6663b5
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-09 15:37:08 +0800

    modify to auth db production

--- models/conndb.js
+++ models/conndb.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // create connection config
 const con = mysql.createConnection({
-  host: "localhost", // your db host (type full url if from other server)
-  user: "root", // your db username
-  password: "", // your db password
-  database: "project_node_db" // your db name
+  host: process.env.HOSTNAME, // "localhost", // your db host (type full url if from other server)
+  user: process.env.USERNAME, // "root", // your db username
+  password: process.env.PASSWORD, // "", // your db password
+  database: process.env.DATABASE, // "project_node_db" // your db name
 });
 
 // establish connection database
