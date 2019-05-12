144bfe62043859f1e81aacf86a5888441cc1a3b6
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-12 14:43:10 +0800

    update reconnect

--- models/conndb.js
+++ models/conndb.js
@@ -1,24 +1,24 @@
 const mysql = require('mysql');
 
-// create connection config
-const con = mysql.createConnection({
-  host: process.env.HOSTNAME || "localhost", // your db host (type full url if from other server)
-  port: 3306, // your db port number (default is 3306)
-  user: process.env.USERNAME || "root", // your db username
-  password: process.env.PASSWORD || "", // your db password
-  database: process.env.DATABASE || "project_node_db", // your db name
-});
-
 function handleReconnect(con) {
+  // create connection config
+  const con = mysql.createConnection({
+    host: process.env.HOSTNAME || "localhost", // your db host (type full url if from other server)
+    port: 3306, // your db port number (default is 3306)
+    user: process.env.USERNAME || "root", // your db username
+    password: process.env.PASSWORD || "", // your db password
+    database: process.env.DATABASE || "project_node_db", // your db name
+  });
+
   // establish connection database
-  return con.connect(function(err) {
+  con.connect(function(err) {
     if (err) {
       console.log(err);
-      setTimeout(handleReconnect(con), 2000);
+      setTimeout(handleReconnect(), 2000);
     }
     console.log("Connected ..");
   });
 };
-handleReconnect(con);
+handleReconnect();
 
 module.exports = con;
